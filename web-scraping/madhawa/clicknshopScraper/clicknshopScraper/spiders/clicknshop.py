834c4820fdc0def8706603c3a61e75e5ba08e5ee
Madhawa Manchanayake <36152838+Madlhawa@users.noreply.github.com>
2018-09-10 22:20:17 +0530

    polished

--- web-scraping/madhawa/clicknshopScraper/clicknshopScraper/spiders/clicknshop.py
+++ web-scraping/madhawa/clicknshopScraper/clicknshopScraper/spiders/clicknshop.py
@@ -6,6 +6,7 @@ import unicodedata
 from re import sub
 from decimal import Decimal
 import glob
+import money-parser
 
 
 class ClicknshopSpider(scrapy.Spider):
@@ -29,7 +30,7 @@ class ClicknshopSpider(scrapy.Spider):
         print('\x1b[1;35;40m' + response.request.url + '\x1b[0m')
         Title = response.xpath('//span[@class="nameCont"]/text()').extract_first()
         #Price = response.xpath('//span[@class="price"]/text()').extract()[1]
-        Price = re.findall("?:[\£\$\€\'Rs.']{1}[,\d]+.?\d*", response.xpath('//span[@class="nameCont"]/text()').extract_first())
+        Price = price_str(response.xpath('//span[@class="price"]/text()').extract()[1])
         #description = list(response.xpath('//*[@id="product-attribute-specs-table"]/tbody/tr[10]/td/text()').extract())
         description = re.sub( '\s+', ' ', unicodedata.normalize("NFKD",''.join(list(response.xpath('//*[@id="product-attribute-specs-table"]/tbody/tr[10]/td/text()').extract()))) ).strip()
         img = response.xpath('//*[@id="image-0"]/@src').extract_first()
