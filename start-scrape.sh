11c26f689210159e8d06353f21124e128f0edfc6
Madlhawa <madhawa242@gmail.com>
2018-09-14 13:41:51 +0000

    Automated scrape

--- start-scrape.sh
+++ start-scrape.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 source /home/madhawa242/python-environments/scrapy_env/bin/activate
 cd web-scraping/madhawa/kaprukascraper/kaprukascraper/;
-scrapy crawl kapruka;
+#scrapy crawl kapruka;
 cd ../../buyabans/buyabans/;
-scrapy crawl buyabansbot;
+#scrapy crawl buyabansbot;
 cd ../../clicknshopScraper/clicknshopScraper/;
-scrapy crawl clicknshop;
+#scrapy crawl clicknshop;
 cd ../../scionscraper/scionscraper/;
-scrapy crawl scion;
+#scrapy crawl scion;
 cd ../../daraz/daraz/;
-scrapy crawl darazbot;
-cd../../ikman/ikman;
+#scrapy crawl darazbot;
+cd ../../ikman/ikman;
 scrapy crawl ikmanbot;
 git pull;
 git add .;
