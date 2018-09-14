c06e1082093830097c3ecddcb23d3d1b9cc0371a
Madlhawa <madhawa242@gmail.com>
2018-09-14 20:06:41 +0000

    scraped ikman

--- web-scraping/madhawa/ikman/ikman/settings.py
+++ web-scraping/madhawa/ikman/ikman/settings.py
@@ -22,7 +22,7 @@ FEED_URI = "../../../../data/ikman.json"
 #USER_AGENT = 'ikman (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = false
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
